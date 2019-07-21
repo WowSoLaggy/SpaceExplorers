57f1ccc2da8f6d1ca579dc16a8bec4c07826b10e
WowSoLaggy <dohxehapo@gmail.com>
2019-07-22 00:20:17 +0300

    (-) Draw cursor without camera translation

--- SpaceExplorers/Gui.cpp
+++ SpaceExplorers/Gui.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "Gui.h"
 
+#include <LaggyDx/IRenderer2d.h>
+
 
 Gui::Gui(const Dx::IResourceController& i_resourceController)
   : d_resourceController(i_resourceController)
@@ -27,5 +29,6 @@ void Gui::update(double i_dt)
 
 void Gui::render(Dx::IRenderer2d& i_renderer) const
 {
+  i_renderer.resetTranslation();
   d_cursor.render(i_renderer);
 }
