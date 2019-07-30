d9de63ee1c8ed7af9e2d72072fd5716ef6ec9fa5
WowSoLaggy <dohxehapo@gmail.com>
2019-07-31 00:31:07 +0300

    (=) Commented unused code

--- SpaceExplorers/World.cpp
+++ SpaceExplorers/World.cpp
@@ -26,8 +26,8 @@ void World::update(double i_dt)
   for (auto&[_, tile] : d_tilesMap)
     tile.update(i_dt);
 
-  for (auto& item : d_objects)
-    item->update(i_dt);
+  /*for (auto& item : d_objects)
+    item->update(i_dt);*/
 }
 
 void World::render(Dx::IRenderer2d& i_renderer, const Sdk::RectI& i_viewport) const
