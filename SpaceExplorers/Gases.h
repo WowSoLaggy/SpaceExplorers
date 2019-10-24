fad84b0ca3807171d641dfba3cc194eea675228e
WowSoLaggy <dohxehapo@gmail.com>
2019-10-24 23:35:07 +0300

    (+) Gases' names

--- SpaceExplorers/Gases.h
+++ SpaceExplorers/Gases.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 
 enum class Gas : std::int32_t
 {
   Oxygen = 0,
+};
+
+const std::unordered_map<Gas, std::string> GasNames{
+  { Gas::Oxygen, "O2" },
 };
